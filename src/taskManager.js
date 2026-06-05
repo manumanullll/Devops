25760a9558e06a0051783bc37d4dd55bef4e66ea
manumanullll <manuela.dewes@iesb.edu.br>
2026-06-05 16:51:58 -0300

    green: implementa filterTasks

--- src/taskManager.js
+++ src/taskManager.js
@@ -1,3 +1,17 @@
 export function removeTask(tasks, taskId) {
-    return tasks.filter(task => task.id !== taskId);
+    return tasks.filter((task) => task.id !== taskId);
 }
+
+export function filterTasks(tasks, status) {
+    switch (status) {
+        case 'completed':
+            return tasks.filter((task) => task.completed === true);
+        case 'pending':
+            return tasks.filter((task) => task.completed === false);
+        case 'all':
+        default:
+            return [...tasks];
+    }
+}
+
+export const filterTask = filterTasks;
